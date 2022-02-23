87a8b2ab8ffb15d9621bff7a520146e071f0fecc
ValentinaSempt <99371623+ValentinaSempt@users.noreply.github.com>
2022-02-23 13:25:30 -0400

    Create triangulo .h

---
+++ triangulo .h
@@ -0,0 +1,14 @@
+# pragma una vez
+clase  triangulo
+{
+	// caracteristicas
+privado:
+	base flotante ;
+	altura del flotador ;
+	área de flotación ;
+	// métodos - procedimientos - prototipos
+público:
+	Triángulo ( vacío );
+	~ Triángulo ( vacío );
+	 área de cálculo flotante ( flotante b, flotante h);
+};
